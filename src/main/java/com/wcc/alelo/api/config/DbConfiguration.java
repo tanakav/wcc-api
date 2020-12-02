3fb51c2e170374b44c11c49aa06f91ab9415effa
yukimochii <tanakavv15@gmail.com>
2020-12-02 00:02:34 -0300

    Adicionado property config de dev

---
+++ src/main/java/com/wcc/alelo/api/config/DbConfiguration.java
@@ -0,0 +1,59 @@
+package com.wcc.alelo.api.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties("spring.datasource")
+public class DbConfiguration {
+
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Profile("dev")
+    @Bean
+    public String testeDatabaseConnection(){
+        System.out.println("DB connection for DEV - h2");
+        System.out.println(driverClassName);
+        System.out.println(url);
+
+        return "DB connection to H2_TEST";
+
+    }
+}
